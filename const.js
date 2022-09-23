676e510052b8e3ea6bccb2d02e703d0b4addbc8c
Arkadii Shyn <arkadiy.shin@gmail.com>
2022-09-23 14:01:14 +0200

    added constans

---
+++ const.js
@@ -0,0 +1,17 @@
+export const REC_PREFIX = 'recognittion_';
+
+export const RECOGNITION_LANGUGES = [
+    {id: 'eng', alias: '🇺🇸 en'},
+    {id: 'deu', alias: '🇩🇪 de'},
+    {id: 'ukr', alias: '🇺🇦 ua'},
+    {id: 'rus', alias: '🇷🇺 ru'},
+
+]
+
+
+export const TRANSLATION_LANGUGES = [
+    {id: 'en', alias: '🇺🇸 en'},
+    {id: 'de', alias: '🇩🇪 de'},
+    {id: 'uk', alias: '🇺🇦 ua'},
+    {id: 'ru', alias: '🇷🇺 ru'},
+]
