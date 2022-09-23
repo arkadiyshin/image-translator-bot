1e2b6c4a320c708f15bd4b9c00b4fbd2604209cd
Arkadii Shyn <arkadiy.shin@gmail.com>
2022-09-23 16:09:05 +0200

    added hindi lang

--- const.js
+++ const.js
@@ -4,6 +4,7 @@ export const RECOGNITION_LANGUGES = [
     {id: 'eng', alias: '🇺🇸 en'},
     {id: 'deu', alias: '🇩🇪 de'},
     {id: 'ukr', alias: '🇺🇦 ua'},
+    {id: 'hin', alias: '🇮🇳 hi'},
     {id: 'rus', alias: '🇷🇺 ru'},
 
 ]
@@ -13,5 +14,6 @@ export const TRANSLATION_LANGUGES = [
     {id: 'en', alias: '🇺🇸 en'},
     {id: 'de', alias: '🇩🇪 de'},
     {id: 'uk', alias: '🇺🇦 ua'},
+    {id: 'hi', alias: '🇮🇳 hi'},
     {id: 'ru', alias: '🇷🇺 ru'},
 ]
